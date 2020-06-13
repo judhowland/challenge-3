ed3b32da57827f188069252600cf06da127361d7
Jud <jud.howland@gmail.com>
2020-06-13 13:06:37 -0500

    Updated code again

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,36 +15,32 @@ var generateBtn = document.querySelector("#generate");
 // Function to get criteria
 var selectCriteria = function () {
   var passLength = prompt("How long would you like your password to be? Must be between 8 and 128 characters");
-  if (passLength < 8) {
+  if (passLength < 8 || passLength > 128)  {
     alert("Please choose a valid length.");
     return;
-  }
-  if (passLength > 128) {
-    alert("Please choose a valid length.");
-    return;
-  }
+  };
 
-  var passUppercase = prompt("Would you like your password to include uppercase letters?");
+  var passUppercase = confirm("Would you like your password to include uppercase letters?");
   if (passUppercase === true) {
     alert("Password will include uppercase letters");
   }
 
-  var passLowercase = prompt("Would you like your password to include lowercase letters?");
+  var passLowercase = confirm("Would you like your password to include lowercase letters?");
   if (passLowercase === true) {
     alert("Password will include lowercase letters");
   }
 
-  var passNumeric = prompt("Would you like your password to include numbers?");
+  var passNumeric = confirm("Would you like your password to include numbers?");
   if (passNumeric === true) {
     alert("Password will include numbers");
   }
 
-  var passSpecialChars = prompt("Would you like your password to include special characters?");
+  var passSpecialChars = confirm("Would you like your password to include special characters?");
   if (passSpecialChars === true) {
     alert("Password will include special characters");
   }
 
-  if (passLength === false && passUppercase === false && passLowercase === false && passNumeric === false && passSpecialChars === false) {
+  if (passUppercase === false && passLowercase === false && passNumeric === false && passSpecialChars === false) {
     alert("You need to pick at least one criteria");
     return;
   } 
@@ -65,14 +61,17 @@ function generateRandom(arr) {
   return randomChar;
 };
 
+
 function generatePassword() {
   var criteria = selectCriteria();
   var result = [];
   var possibleChars = [];
   var guaranteedChars = [];
   console.log(criteria);
-  console.log(result);
-  console.log(possibleChars);
+
+  if (!criteria) {
+    return;
+  }
 
   if (criteria.specialChars) {
     possibleChars = possibleChars.concat(specialCharsArray);
@@ -94,15 +93,23 @@ function generatePassword() {
     guaranteedChars.push(generateRandom(numericArray));
   }
 
+  console.log(possibleChars);
+
   for (var i = 0; i < criteria.length; i++) {
-    var possibleCharacters = generateRandom(possibleChars);
+    debugger;
+    var possibleCharsArray = possibleChars.split("");
+    console.log(possibleCharsArray);
+    var possibleCharacters = generateRandom(possibleCharsArray);
     result.push(possibleCharacters);
+    console.log(possibleCharacters);
   }
 
   // mixing at least one guaranteed character
   for (var i = 0; i < guaranteedChars.length; i++) {
     result[i] = guaranteedChars[i];
+    console.log(result[i]);
   }
+
   return result.join("");
 };
 
@@ -110,6 +117,9 @@ function generatePassword() {
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  if (!password) {
+    return;
+  }
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
